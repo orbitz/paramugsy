135b1314dc1a06cdc1cf9a7487c27f29bd6ccbc5
Malcolm Matalka <mmatalka@gmail.com>
2011-11-11 20:49:21 +0000

    Putting paramugsy dir infront of the PATH in template file

--- scripts/pm_qsub_template.sh
+++ scripts/pm_qsub_template.sh
@@ -4,7 +4,9 @@ export MUGSY_INSTALL=$PWD/mugsy_x86-64
 export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping
 export PERL5LIB=$MUGSY_INSTALL/perllibs
 export PATH=$PATH:$MUGSY_INSTALL/MUMmer3.20
-export PATH=$PATH:/opt/paramugsy
+
+# We want paramugsy infront of everything because we are putting modified binaries in here
+export PATH=/opt/paramugsy:$PATH
 mkdir -p /mnt/tmp
 cd /mnt/tmp
 
