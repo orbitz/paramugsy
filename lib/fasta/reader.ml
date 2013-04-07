27b127c5d2e2bcd2e45d61c80065f32f877ea656
orbitz <orbitz@gmail.com>
2013-04-07 15:39:45 +0200

    Add reader for fasta

--- lib/fasta/reader.ml
+++ lib/fasta/reader.ml
@@ -1,88 +1,136 @@
 open Core.Std
 
-type buffer =
-  | Header   of string
-  | Seq      of string
-  | Seq_done of string
-
-type t = { mutable buf  : buffer
-	 ;         chan : In_channel.t
+type t = { in_chan     : In_channel.t
+	 ; buf         : string
+	 ; mutable len : int
 	 }
 
-let rec find_next_header chan =
-  match In_channel.input_line chan with
-    | None ->
-      None
-    | Some s when s.[0] = '>' ->
-      Some s
-    | Some _ ->
-      find_first_header chan
+let buf_size = 4096
 
-let create chan =
-  match find_next_header chan with
-    | None ->
-      Error `Bad_format
-    | Some header ->
-      Ok { buf  = ref (Header header)
-	 ; chan = chan
-	 }
+let create in_chan = { in_chan; buf = String.create buf_size; len = 0 }
 
-let read_next_header = function
-  | { buf = Header h } ->
-    Some h
-  | { buf = Seq_done h } as t -> begin
-    t.buf <- Header h;
-    Some h
+let rec read_max in_chan ~buf ~pos ~len =
+  match In_channel.input in_chan ~buf ~pos ~len with
+    | 0 -> 0
+    | n when n = len -> n
+    | n -> n + read_max in_chan ~buf ~pos:(pos + n) ~len:(len - n)
+
+let find_char s ~pos ~len ~c =
+  match String.index_from s pos c with
+    | Some idx when idx < (pos + len) -> Some idx
+    | _                               -> None
+
+let find_begin_header s ~pos ~len =
+  find_char s ~pos ~len ~c:'>'
+
+let find_endline s ~pos ~len =
+  find_char s ~pos ~len ~c:'\n'
+
+let move_buf_left t ~pos =
+  String.blit
+    ~src:t.buf
+    ~src_pos:pos
+    ~dst:t.buf
+    ~dst_pos:0
+    ~len:(t.len - pos);
+  t.len <- t.len - pos
+
+let read_buf_max t =
+  if t.len < buf_size then begin
+    match
+      read_max
+	t.in_chan
+	~buf:t.buf
+	~pos:t.len
+	~len:(buf_size - t.len)
+    with
+      | 0 -> 0
+      | n -> begin t.len <- t.len + n; n end
   end
-  | { buf = Seq _ } as t -> begin
-    match find_next_header t.chan with
-      | None ->
-	None
-      | Some header -> begin
-	t.buf <- Header header;
-	Some header
-      end
-  end
+  else
+    0
 
-
-let read_next_seq_line t s =
-  match In_channel.input_line t.chan with
-    | None ->
-      t.buf <- Seq_fin s
-    | Some str when str.[0] = '<' ->
-      t.buf <- Seq-fun
-    | Some str ->
-      t.buf <- Seq (s ^ str);
-
-(*
- * This interface doesn't make much sense given the
- * implementation but it is this way so the
- * implementation can change to be more peformant
- * without changing the code of the callers
- *)
-val rec read_next_seq t ~buf ~pos ~len =
-  match t with
-    | { buf = Header _ } as t -> begin
-      match In_channel.input_line t.chan with
-	| None ->
-	  0
-	| Some s ->
-	  read_next_seq
-	    { t with buf = Seq s }
-	    ~buf
-	    ~pos
-	    ~len
+let rec next_header t =
+  match find_begin_header t.buf ~pos:0 ~len:t.len with
+    | None -> begin
+      (*
+       * If there is no sign of a a header, throw everything
+       * away and read the next chunk in
+       *)
+      t.len <- 0;
+      match read_buf_max t with
+	| 0 -> None
+	| _ -> next_header t
     end
-    | { buf = Seq s } as t when len <= String.length s -> begin
+    | Some hstart -> begin
+      match find_endline t.buf ~pos:hstart ~len:(t.len - hstart) with
+	| Some hend -> begin
+	  let header = String.sub t.buf ~pos:(hstart + 1) ~len:(hend - hstart - 1) in
+	  move_buf_left t ~pos:(hend + 1);
+	  Some header
+	end
+	| None -> begin
+	  move_buf_left t ~pos:hstart;
+	  match read_buf_max t with
+	    | 0 -> begin
+	      let header = String.sub t.buf ~pos:(hstart + 1) ~len:(t.len - hstart - 1) in
+	      t.len <- 0;
+	      Some header
+	    end
+	    | _ ->
+	      next_header t
+	end
+    end
+
+let strip_bad_chars buf len =
+  let rec sbc' i dropped =
+    if i < len then begin
+      if Char.is_whitespace buf.[i] then
+	sbc' (i + 1) (dropped + 1)
+      else if dropped > 0 then begin
+	buf.[i - dropped] <- buf.[i];
+	sbc' (i + 1) dropped
+      end
+      else
+	sbc' (i + 1) dropped
+    end
+    else
+      len - dropped
+  in
+  sbc' 0 0
+
+let rec next_seq t ~buf ~pos ~len =
+  match find_begin_header t.buf ~pos:0 ~len with
+    | Some idx -> begin
       String.blit
-	~src:s
+	~src:t.buf
 	~src_pos:0
 	~dst:buf
 	~dst_pos:pos
-	~len:len;
-      t.buf <- Seq (String.drop_prefix s len);
-      len
+	~len:idx;
+      move_buf_left t ~pos:idx;
+      strip_bad_chars buf idx
     end
-    | { buf = Seq s } as t -> begin
-      match
+    | None when t.len > len -> begin
+      String.blit
+	~src:t.buf
+	~src_pos:0
+	~dst:buf
+	~dst_pos:pos
+	~len;
+      move_buf_left t ~pos:len;
+      strip_bad_chars buf len
+    end
+    | None -> begin
+      let src_len = t.len in
+      String.blit
+	~src:t.buf
+	~src_pos:0
+	~dst:buf
+	~dst_pos:pos
+	~len:src_len;
+      move_buf_left t ~pos:t.len;
+      match read_buf_max t with
+	| 0 -> strip_bad_chars buf src_len
+	| _ -> next_seq t ~buf ~pos:(pos + src_len) ~len:(len - src_len)
     end
--- lib/fasta/reader.mli
+++ lib/fasta/reader.mli
@@ -2,25 +2,6 @@ open Core.Std
 
 type t
 
-type error = [ `Bad_format ]
-
-(*
- * This reads the fasta file upto the first header
- *)
-val create : In_channel.t -> (t, [> error ]) Result.t
-
-(*
- * Read upto the next header and return it.  This function is
- * idempotent.
- *)
-val read_next_header : t -> string option
-
-(*
- * Read a sequence upto a header
- * Return 0 when there is no more of the sequence to read.
- * otherwise return the amount that has been read.
- * When a sequence has been finished, consecutive calls to
- * this will return 0 until [read_next_header] has been
- * called
- *)
-val read_next_seq : t -> buf:string -> pos:int -> len:int -> int
+val create      : In_channel.t -> t
+val next_header : t -> string option
+val next_seq    : t -> buf:string -> pos:int -> len:int -> int
